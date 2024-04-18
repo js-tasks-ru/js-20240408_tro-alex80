d5b0666a8d247cc47600711619be343c572655ef
vm-alextr80 <tro.alex80@gmail.com>
2024-04-18 16:43:12 +0100

    3 lesson task done

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,26 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) return;
+  const arr = Object.entries(obj);
 
+  return arr.reduce((res, [key, value]) => {
+    res[value] = key;
+    return res;
+  }, {});
 }
+
+// export function invertObj(obj) {
+//   if (!obj) return;
+//   const result = Object.entries(obj);
+//   let object = {};
+//
+//   for (const key of result) {
+//     object[key[1]] = key[0];
+//   }
+//   return object;
+// }
+//
+// const obj = { key: 'value' };
+//
+// console.log(invertObj(obj)); // { value: 'key'}
